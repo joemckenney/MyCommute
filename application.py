3e555a8d09eef04128f5d53295e39a1b75be5cd2
Seth Boyles <sethboyles@gmail.com>
2013-02-16 15:58:54 -0800

    index template with vars

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('python/index.html')
+    return render_template('python/index.html', require=url_for('static', filename='js/require.js'))
+
 
 if __name__ == '__main__':
     app.run()
