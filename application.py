ff7148e96c00730d67c1650343b8c877b23b1144
Joseph McKenney <joe@jmckenney.local>
2013-02-23 16:54:43 -0800

    creating the bart models and flask endpoints, initial architecture for BART api

--- application.py
+++ application.py
@@ -3,12 +3,43 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route('/bart/advisory')
+def advisory():
+    # 
+    # http://api.bart.gov/api/bsa.aspx
+    #
+    return 'bart system advisory'
 
 
-@app.route('/bart')
-def bart():
-    return 'suck it joe'
+@app.route('/bart/etd')
+def advisory():
+    # 
+    # http://api.bart.gov/api/etd.aspx
+    #
+    return 'bart estimated time of departure'
 
+@app.route('/bart/routes')
+def routes():
+    # 
+    # http://api.bart.gov/api/routes.aspx
+    #
+    return 'bart route(s) information'
+
+
+@app.route('/bart/schedule')
+def schedule():
+    # 
+    # http://api.bart.gov/api/sched.aspx
+    #
+    return 'bart schedule information'
+
+
+@app.route('/bart/station')
+def station():
+    # 
+    # http://api.bart.gov/api/stn.aspx
+    #
+    return 'bart station information'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
---
+++ static/js/models/bart/ETD.js
@@ -0,0 +1,19 @@
+define(
+    [
+        'jquery',
+        'underscore',
+        'backbone'
+    ],
+    function($, _, Backbone){
+        //replace with Base Model
+        return Backbone.Model.extend({
+            //bart system advisory
+            url: '/bart/etd'
+            initialize: function() {
+                
+            }
+        });
+        
+
+    }
+);
